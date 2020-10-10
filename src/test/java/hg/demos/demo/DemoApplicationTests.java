f71e567f865f03a2e95ab977cd7016759d3a6848
Hassen <gaaya.dev@gmail.com>
2020-10-10 21:21:12 +0200

    Init project Spring Jenkins

---
+++ src/test/java/hg/demos/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.demos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

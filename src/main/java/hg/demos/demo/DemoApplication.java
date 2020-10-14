01895bce8317c78965bab784efdd6943d61897d9
Hassen <gaaya.dev@gmail.com>
2020-10-14 23:18:43 +0200

    Changer le port et ajouter un controlleur

--- src/main/java/hg/demos/demo/DemoApplication.java
+++ src/main/java/hg/demos/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package hg.demos.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		@RequestMapping(value = "/home")
+		public String getWelcomeMessage() {
+			return "Welcome";
+		}
 
-}
+		public static void main(String[] args) {
+			SpringApplication.run(DemoApplication.class, args);
+		}
+
+	}

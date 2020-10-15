e964bd9b3f44f722af84d353492c965f8c1f5b0e
Hassen <gaaya.dev@gmail.com>
2020-10-15 23:15:52 +0200

    Ajouter un test unitaire

--- src/test/java/hg/demos/demo/DemoApplicationTests.java
+++ src/test/java/hg/demos/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package hg.demos.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sun.tools.javac.util.Assert;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Boolean toTest = false;
+		assert toTest == false;
 	}
 
 }
